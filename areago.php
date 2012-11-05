ef21607836fa5e2e3af7c7cf3a399b78fad78a9c
Xavier Balderas <xavibalderas@gmail.com>
2012-11-05 16:49:03 +0100

    Plugin activo

---
+++ areago.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: Areago Web Interface
+Plugin URI: http://www.audio-lab.org
+Description: Plugin para la gesti—n de audioguias basado en el software Areago.
+Version: 1.0
+Author: Xavier Balderas
+Author URI: http://www.audio-lab.org
+License: GPL2
+*/
+
+
